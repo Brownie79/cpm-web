76bcf733d7c696aaf62e7ee31ba0efb4557aead0
brownie79 <icarus@localhost.localdomain>
2016-12-03 21:23:19 -0600

    jquery fails to load now

--- app/components/addConfPage/addConf.component.ts
+++ app/components/addConfPage/addConf.component.ts
@@ -4,13 +4,20 @@ import { Component } from '@angular/core';
     moduleId: module.id,
     selector: 'addConf',
     templateUrl: 'addConf.html'
-})
+});
 
 export class AddConfPage {
     constructor(){
         //console.log("addConf loaded");
     }
- } 
 
+    /*ngAfterViewInit(){
+        /*window['jQuery']('.datepicker').pickadate({
+            selectMonths: true,
+            selectYears: 15
+        });*/
+    }*/
+ } 
+ 
 
 //<li class="g-signin2" data-onsuccess="onSignIn"></li>
